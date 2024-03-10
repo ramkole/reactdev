96b22830731a8cae2344663b21266ad2f5394e61
ramkole <ram_kole@yahoo.com>
2024-03-10 20:12:28 +0530

    dotted grid background css added

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body
+        className={`${inter.className} absolute inset-0 h-full w-full bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]`}
+      >
         <FloatingNav navItems={navItems} />
         <div className="flex flex-col min-h-[100dvh] pt-14">
           <HeaderHero />
